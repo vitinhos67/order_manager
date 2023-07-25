e84e6b6c16612edfd4c8e2098ccb962597a49565
Victor Santos <santtosvictor42@gmail.com>
2023-07-25 17:08:16 -0300

    chore: first commit

---
+++ src/test/java/com/ordermanager/orderManager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordermanager.orderManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
