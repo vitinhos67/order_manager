6995b66830853f73f4b794b7d391a7a51b15ba00
Victor Santos <santtosvictor42@gmail.com>
2023-10-19 08:45:45 -0300

    feat(config cache): create bean with configs from redis cache

--- src/main/java/com/ordermanager/configs/CacheConfig.java
+++ src/main/java/com/ordermanager/configs/CacheConfig.java
@@ -1,17 +1,31 @@
 package com.ordermanager.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Configuration
 public class CacheConfig {
+
+
+    @Bean
+    public RedisCacheConfiguration redisConfig(ObjectMapper objectMapper) {
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+
+            return config
+                .entryTtl(Duration.ofMinutes(120))
+                .disableCachingNullValues();
+    }
 
     @Bean
     public CacheManager cacheManager() {
